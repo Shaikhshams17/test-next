61d97f10972a1abeccf8636d1d02c517ed0c1a66
Shams Ali <shaikhshams59@gmail.com>
2025-08-13 16:31:46 +0530

    rename 1 file, delete 1 file and create 1 file

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+   <div className="flex items-center justify-center h-screen">
+    <h1 className="text-4xl font-extrabold">Shams Ali Shaikh</h1>
+   </div>
+  );
+}
+
